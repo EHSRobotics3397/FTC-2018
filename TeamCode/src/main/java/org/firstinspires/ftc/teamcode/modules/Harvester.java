f75e322dfee0ed0e4dd5c1d8fd5956b186c159cb
Essex Robotics-1 <essex3397@gmail.com>
2018-11-10 15:30:00 -0500

    Created class to control Harvester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Harvester.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.modules;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+/* A Class Used to control Harvester on robot,
+    Created by EHS Robotics on 11/10/18
+    with code from 2017 season
+ */
+public class Harvester {
+    //Set up motors and isGold
+    private DcMotor harvesterMotor;
+    private boolean takeGold;
+}
