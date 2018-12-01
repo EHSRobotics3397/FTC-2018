4efc037a60863fe7b1eca6a9b2c720bb8b1cb40c
Essex Robotics-1 <essex3397@gmail.com>
2018-12-01 13:50:10 -0500

    Worked more on parabola class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/positioning/Parabola.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/positioning/Parabola.java
@@ -1,14 +1,32 @@
 package org.firstinspires.ftc.teamcode.modules.positioning;
-
+// not done
 public class Parabola {
     // stores the 3 numbers modifying x as double array
     // It only uses addition but numbers can be negative
     private double[] equation;
+    private boolean possible;
     private Coordinates root1;
     private Coordinates root2;
     private Coordinates vertex;
 
     public Parabola (Coordinates rt1, Coordinates rt2, Coordinates vrtx) {
-        
+        root1 = rt1;
+        root2 = rt2;
+        vertex = vrtx;
+
+        // check if 3 points given are colinear
+        Line test1 = new Line(root1, root2);
+        Line test2 = new Line(root2, vertex);
+
+        if (test1.getSlope() == test2.getSlope()) {
+            possible = false;
+        } else {
+            possible = true;
+        }
+
+    }
+
+    public boolean isPossible () {
+        return possible;
     }
 }
