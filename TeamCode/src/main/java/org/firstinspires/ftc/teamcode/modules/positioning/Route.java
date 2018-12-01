b00e357170ab6cd28954a76f1d7707635190db75
Essex Robotics-1 <essex3397@gmail.com>
2018-12-01 15:05:51 -0500

    Made route class able to create list of route segs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/positioning/Route.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/positioning/Route.java
@@ -1,5 +1,19 @@
 package org.firstinspires.ftc.teamcode.modules.positioning;
 
-public class Route {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Route {
+    public List<RouteSeg> segs;
+    private Coordinates start;
+    private Coordinates end;
+
+    public Route (Coordinates st, Coordinates en) {
+        start = st;
+        end = en;
+
+        segs = new ArrayList<RouteSeg>(1);
+        RouteSeg tmpSeg = new RouteSeg(start, end, null, RouteSeg.IS_LINE);
+        segs.get(0) = tmpSeg;
+    }
 }
